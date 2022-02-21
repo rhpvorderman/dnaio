7ec38e97fd84d40039e798f0fd7af832c2a533ee
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-02-21 15:54:59 +0100

    Start on vectorizing

--- src/dnaio/ascii_check.h
+++ src/dnaio/ascii_check.h
@@ -3,29 +3,36 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdalign.h>
+#include <x86intrin.h>
+
+static const alignas(128) uint64_t ascii_mask_16_byte[2] = {ASCII_MASK_8BYTE, ASCII_MASK_8BYTE};
 
 static int
 string_is_ascii(char * string, size_t length) {
     size_t n = length;
     char * char_ptr = string;
+    typedef __m128i longword;
+    longword *longword_ascii_mask = (longword *)ascii_mask_16_byte;
+
     // The first loop aligns the memory address. Char_ptr is cast to a size_t
-    // to return the memory address. Uint64_t is 8 bytes long, and the processor
+    // to return the memory address. longword is 8 bytes long, and the processor
     // handles this better when its address is a multiplier of 8. This loops
     // handles the first few bytes that are not on such a multiplier boundary.
-    while ((size_t)char_ptr % sizeof(uint64_t) && n != 0) {
+    while ((size_t)char_ptr % sizeof(longword) && n != 0) {
         if (*char_ptr & ASCII_MASK_1BYTE) {
             return 0;
         }
         char_ptr += 1;
         n -= 1;
     }
-    uint64_t *longword_ptr = (uint64_t *)char_ptr;
-    while (n >= sizeof(uint64_t)) {
-        if (*longword_ptr & ASCII_MASK_8BYTE){
+    longword *longword_ptr = (longword *)char_ptr;
+    while (n >= sizeof(longword)) {
+        if (!_mm_testz_si128(*longword_ptr, *longword_ascii_mask)){
             return 0;
         }
         longword_ptr += 1;
-        n -= sizeof(uint64_t);
+        n -= sizeof(longword);
     }
     char_ptr = (char *)longword_ptr;
     while (n != 0) {
