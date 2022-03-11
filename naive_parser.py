101faf36906cb7a9efbe3e8881da9f89a2f9ee2e
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-03-11 14:42:44 +0100

    Add naive parser for comparison

---
+++ naive_parser.py
@@ -0,0 +1,42 @@
+import sys
+import io
+
+
+class FastqRecord:
+    name: str
+    sequence: str
+    qualities: str
+
+    def __init__(self, name, sequence, qualities):
+        if len(sequence) != len(qualities):
+            raise ValueError("Sequence and qualities must be of the same length.")
+
+    def fastq_bytes(self):
+        return f"@{self.name}\n{self.sequence}\n+\n{self.qualities}\n".encode("ascii")
+
+
+def fastq_iter(filename: str):
+    with open(filename, "rt", encoding="ascii") as file:
+        while True:
+            name = file.readline()
+            if not name:
+                return
+            if not name.startswith("@"):
+                raise ValueError("FASTQ record should start with @.")
+            sequence = file.readline()
+            second_header = file.readline()
+            qualities = file.readline()
+            if not (sequence and second_header and qualities):
+                raise EOFError(f"Truncated FASTQ file at {name}")
+            if not second_header.startswith("+"):
+                raise ValueError("Second header should start with +.")
+            name = name[1:-1]  # strip @ and \n
+            sequence = sequence[:-1]
+            qualities = qualities[:-1]
+            # Length check in FastqRecord init.
+            yield FastqRecord(name, sequence, qualities)
+
+
+if __name__ == "__main__":
+    for record in fastq_iter(sys.argv[1]):
+        pass
