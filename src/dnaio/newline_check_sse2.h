f92eba0e67e6e347e937be6cd5bac95c3c07ece2
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-11-16 11:56:11 +0100

    Further optimization of find_newline

--- src/dnaio/newline_check_sse2.h
+++ src/dnaio/newline_check_sse2.h
@@ -1,20 +1,31 @@
 #include <string.h>
 #include <emmintrin.h>
-static inline int contains_newline(const char *restrict string, size_t length) {
+#include <stdint.h>
+
+static inline int contains_newline(const char *string, size_t length) {
     __m128i allcomps = _mm_setzero_si128();
     __m128i all_newlines = _mm_set1_epi8('\n');
     size_t remaining = length;
     const char *cursor = string; 
     __m128i current;
+    uint64_t comp = 0;
+    uint8_t tmp;
+    while (remaining > 0 && ((size_t)cursor % sizeof(__m128i))) {
+        tmp = ((((*cursor ^ '\n') - 1) & ~'\n') & (1 << 7));
+        comp |= tmp;
+        remaining -= 1;
+        cursor +=1;
+    }
     while(remaining > sizeof(__m128i)) {
-        current = _mm_loadu_si128((__m128i *)cursor);
+        current = _mm_load_si128((__m128i *)cursor);
         allcomps = _mm_or_si128(allcomps, _mm_cmpeq_epi8(current, all_newlines));
         remaining -= sizeof(__m128i);
         cursor += sizeof(__m128i);
     }
-    int comp = _mm_movemask_epi8(allcomps);
-    while (remaining > 0) {
-        comp |= (*cursor == '\n');
+    comp |= _mm_movemask_epi8(allcomps);
+    while (remaining) {
+        uint8_t tmp = ((((*cursor ^ '\n') - 1) & ~'\n') & (1 << 7));
+        comp |= tmp;
         remaining -= 1;
         cursor +=1;
     }
