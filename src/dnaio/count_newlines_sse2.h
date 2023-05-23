78174ac701be61f5691e6a0cf2b3c4b90d83f3d9
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-05-23 10:42:24 +0200

    Make sure accumulator works correctly

--- src/dnaio/count_newlines_sse2.h
+++ src/dnaio/count_newlines_sse2.h
@@ -1,5 +1,6 @@
 #include "emmintrin.h"
 #include <stdint.h>
+
 size_t count_newlines(const char *text, size_t text_size) {
     const char *cursor = text;
     const char *end_ptr = text + text_size;
@@ -11,7 +12,6 @@ size_t count_newlines(const char *text, size_t text_size) {
         }
         cursor += 1;
     }
-    __m128i newlines = _mm_set1_epi8('\n');
     while (cursor < (end_ptr - sizeof(__m128i))) {
         size_t chunks_remaining = (end_ptr - cursor) / sizeof(__m128i);
         if (chunks_remaining > UINT8_MAX) {
@@ -21,11 +21,10 @@ size_t count_newlines(const char *text, size_t text_size) {
         __m128i accumulator = _mm_setzero_si128();
         for (size_t i=0; i<chunks_remaining; i++) {
             __m128i data = _mm_load_si128((__m128i *)cursor);
-            __m128i eq = _mm_cmpeq_epi8(data, newlines);
-            // cmp_eq sets the most significant bit if a newline is found. 
-            // convert this to a one.
-            // 0b1000_0000 >> 7 = 0b0000_0001 
-            __m128i ones = _mm_srli_si128(eq, 7);
+            __m128i eq = _mm_cmpeq_epi8(data, _mm_set1_epi8('\n'));
+            // cmp_eq sets all bits if a newline is found.
+            // convert this to a one. 255 - 254 = 1
+            __m128i ones = _mm_subs_epu8(eq, _mm_set1_epi8(254));
             accumulator = _mm_adds_epu8(accumulator, ones);
             cursor += sizeof(__m128i);
         }
